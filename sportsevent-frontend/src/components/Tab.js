775b8c407a6690c1b3a7f3ae43786fb50d1b1115
adi_8985 <venkataadityapavantripasuri@my.unt.edu>
2021-10-29 20:14:04 -0500

    added base project for frontend

---
+++ sportsevent-frontend/src/components/Tab.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    //backgroundColor: theme.palette.background.paper,
+    backgroundColor: theme.palette.background.paper,
+  }
+});
+
+class SimpleTabs extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { value } = this.state;
+
+    return (
+      <div className={classes.root}  >
+        <AppBar position="static">
+          <Tabs className={classes.root} value={value} onChange={this.handleChange} centered textColor="primary" indicatorColor="primary">
+            <Tab  label="Ongoing Events" href="#Ongoing Events"/>
+            <Tab  label="Upcoming Events" href="#Upcoming Events"/>
+            <Tab label="Join Event" href="#Join Event" />
+            <Tab label="Discussion Forum" href="#Discussion Forum" />
+            <Tab label="About Us" href="#About us" />
+          </Tabs>
+        </AppBar>
+        {value === 0 && <OngoingEvents/>}
+        {value === 1 && <UpcomingEvents/>}
+        {value === 2 && <JoinEvent/>}
+        {value === 3 && <DiscussionForum/>}
+        {value === 4 && <LastTab/>}
+      </div>
+    );
+  }
+}
+
+SimpleTabs.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SimpleTabs);
